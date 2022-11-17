fb4890552e62638a4391645b555dcbfc76904bc8
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-11-16 23:49:50 -0300

    .

---
+++ server/src/constants.ts
@@ -0,0 +1,5 @@
+export const SECRET = "123adbc"
+export const CLIENT_URL = "https://localhost:3000"
+export const SERVER_URL = "https://localhost:6000"
+export const PORT = "5000"
+
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,12 +1,11 @@
 import express, { Router, Request, Response } from "express";
-const { getUsers , register , login} = require("../controllers/auth");
+const { getUsers, register, login } = require("../controllers/auth");
 const { registerValidation } = require("../validators/auth");
 const { loginValidation } = require("../validators/auth");
 
-const { validationMiddleware } = require("../middlewares/validations-middleware");
-
-
-
+const {
+  validationMiddleware,
+} = require("../middlewares/validations-middleware");
 
 const app = express();
 const cors = require("cors");
@@ -17,18 +16,17 @@ app.use(express.json());
 app.use(cors());
 
 //test route
-route.get('/test', (req, res) => {
-    return res.send("!")
-})
+route.get("/test", (req, res) => {
+  return res.send("!");
+});
 
 //login
-route.post("/login", loginValidation , validationMiddleware, login);
-
+route.post("/login", loginValidation, validationMiddleware, login);
 
 //get users
 route.get("/get-users", getUsers);
 
 //register user
-route.post('/register' , registerValidation, validationMiddleware, register)
+route.post("/register", registerValidation, validationMiddleware, register);
 
 module.exports = route;
