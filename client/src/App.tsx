2faafd0604327239249d1145526f818ffbe49894
FernandoValentim43 <fernandoreal.valentim@gmail.com>
2022-11-21 10:59:00 -0300

    .

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,7 +32,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
 
         <Route element={<PrivateRoutes />}>
           <Route path="/dashboard" element={<Dashboard />} />
